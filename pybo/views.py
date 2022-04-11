fdb88962c3783982a1ebac73aa9fee95c92e7f1e
OSHMOS <ojames9707@gmail.com>
2022-04-11 17:02:29 +0900

    신기한 것 발견 : 현재 question_list.html이 보이면 안 되는데 보이는 중

--- pybo/views.py
+++ pybo/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Question
 
 # Create your views here.
 def index(request):
-    return render(request, 'pybo/index.html')
+    question_list = Question.objects.order_by('-create_date')
+    ctx = {'question_list': question_list}
+    return render(request, 'pybo/question_list.html', ctx)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, id=question_id)
+    ctx = {'question': question}
+    return render(request, 'pybo/question_detail.html', ctx)
+
