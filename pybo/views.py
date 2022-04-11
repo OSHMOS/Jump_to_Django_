6c02e7128ce3d7e9f7ece3ebdab0a6dfc47a3f17
OSHMOS <ojames9707@gmail.com>
2022-04-11 17:12:22 +0900

    프로젝트 : 답변 등록 기능 만들기

--- pybo/views.py
+++ pybo/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 from .models import Question
 
 # Create your views here.
@@ -12,3 +13,8 @@ def detail(request, question_id):
     ctx = {'question': question}
     return render(request, 'pybo/question_detail.html', ctx)
 
+def answer_create(request, question_id):
+    question = get_object_or_404(Question, id=question_id)
+    question.answer_set.create(content=request.POST.get('content'),
+                               create_date=timezone.now())
+    return redirect('pybo:detail', question_id=question.id)
