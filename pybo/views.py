d16029d014984e983dd6ab216f68aa85df89e172
OSHMOS <ojames9707@gmail.com>
2022-04-12 12:57:59 +0900

    프로젝트 : 페이징 기능 구현하기

--- pybo/views.py
+++ pybo/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Question
 from .forms import QuestionForm, AnswerForm
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
+    page = request.GET.get('page', '1')
     question_list = Question.objects.order_by('-create_date')
-    ctx = {'question_list': question_list}
+    paginator = Paginator(question_list, 10)
+    page_obj = paginator.get_page(page)
+    ctx = {'question_list': page_obj}
     return render(request, 'pybo/question_list.html', ctx)
 
 def detail(request, question_id):
