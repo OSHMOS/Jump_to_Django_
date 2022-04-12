e0382433b1007f3ca6e8d25b228517ef62b1d733
OSHMOS <ojames9707@gmail.com>
2022-04-12 12:18:43 +0900

    프로젝트 : 질문 등록 기능, 답변 등록 기능 만들기

--- pybo/views.py
+++ pybo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Question
+from .forms import QuestionForm, AnswerForm
 
 # Create your views here.
 def index(request):
@@ -15,6 +16,28 @@ def detail(request, question_id):
 
 def answer_create(request, question_id):
     question = get_object_or_404(Question, id=question_id)
-    question.answer_set.create(content=request.POST.get('content'),
-                               create_date=timezone.now())
-    return redirect('pybo:detail', question_id=question.id)
+    if request.method == 'POST':
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('pybo:detail', question_id=question_id)
+    else:
+        form = AnswerForm()
+    ctx = {'question': question, 'form': form}
+    return render(request, 'pybo/question_detail.html', ctx)
+
+def question_create(request):
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('pybo:index')
+    else:
+        form = QuestionForm()
+    ctx = {'form': form}
+    return render(request, 'pybo/question_form.html', ctx)
