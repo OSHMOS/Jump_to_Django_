8aa001a491b56d623a727d65151a28891a879992
OSHMOS <ojames9707@gmail.com>
2022-04-13 13:03:45 +0900

    프로젝트 : 로그아웃 구현하기

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 from pybo import views
 
 urlpatterns = [
-    path('', views.index, 'name=index')
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('pybo/', include('pybo.urls')),
     path('common/', include('common.urls')),
